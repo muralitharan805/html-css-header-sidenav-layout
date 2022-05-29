177c7b76686a92434acb4714d44a97e9388e17b7
muralitharan <muralitharan805@gmail.com>
2022-05-29 12:20:59 +0530

    javascript side icon nav

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-console.log('hello!');
+console.log("hello!");
 
 function test() {
-  console.log('hello!');
+  console.log("hello!");
 
-  const test = document.getElementById('test');
+  const test = document.getElementById("test");
 
-  test.style.display = 'none';
+  test.style.display = "none";
+}
+
+let sideNav = false;
+function hidesidenav() {
+  sideNav = !sideNav;
+  console.log("hello");
+  var element = document.getElementById("sidenav-hide");
+  var content = document.getElementById("content-container");
+
+  if (sideNav) {
+    content.classList.remove("content-container-hide-sidenav-icon");
+    element.classList.add("hide-sidenav");
+    content.classList.add("content-container-hide-sidenav");
+  } else {
+    // element.classList.add("show-sidenav");
+    // content.classList.add("content-container-show-sidenav");
+
+    element.classList.remove("hide-sidenav");
+    content.classList.remove("content-container-hide-sidenav");
+  }
+}
+let hideMenuNameCheck = false;
+function hideMenuName() {
+  console.log("hello!");
+
+  hideMenuNameCheck = !hideMenuNameCheck;
+  var getSideNavMenuName = document.getElementsByClassName("menuname");
+  var getContentContainerId = document.getElementById("content-container");
+
+  if (hideMenuNameCheck) {
+    // getSideNavMenuName.style.display = "none";
+
+    for (var x = 0; x < getSideNavMenuName.length; x++) {
+      getSideNavMenuName[x].style.display = "none";
+    }
+    getContentContainerId.classList.add("content-container-hide-sidenav-icon");
+  } else {
+    for (var x = 0; x < getSideNavMenuName.length; x++) {
+      getSideNavMenuName[x].style.display = "block";
+    }
+
+    getContentContainerId.classList.remove(
+      "content-container-hide-sidenav-icon"
+    );
+  }
 }
